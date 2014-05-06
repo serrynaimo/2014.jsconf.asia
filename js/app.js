37eff8985a6bcc87a1c455307d9726acc9e5147f
Raman Shalupau <raman.shalupau@gmail.com>
2014-05-06 11:18:24 +0800

    added sound

--- js/app.js
+++ js/app.js
@@ -45,46 +45,75 @@ $("#subscribeForm .msg").click(function() {
 	$("#subscribeForm input[name='email']").focus();
 });
 
+
+
+var bamSound = (function () {
+	var sound = new Audio('assets/3.mp3');
+	sound.load();
+	return function () {
+		sound.currentTime = 0;
+		sound.play();
+	}
+}());
+var bamSound2 = (function () {
+	var sound = new Audio('assets/2.mp3');
+	sound.load();
+	return function () {
+		sound.currentTime = 0;
+		sound.play();
+	}
+}());
+
 setTimeout(function () {
+	// bamSound();
 	$("#intro").addClass("play");
 	$("#intro .date").addClass("play");
 }, 1000);
 
 setTimeout(function () {
+	bamSound();
 	$("#intro .singapore").addClass("play");
 	window.navigator.vibrate([30,10,20,10,10]);
 }, 1600);
 
 setTimeout(function () {
+	bamSound();
 	$("#intro .venue").addClass("play");
 	window.navigator.vibrate([30,10,20,10,10]);
 }, 2200);
 
 setTimeout(function () {
+	bamSound();
 	$("#intro .js").addClass("play");
 	window.navigator.vibrate([30,10,20,10,10]);
 }, 2800);
 
 setTimeout(function () {
+	bamSound();
 	$("#intro .sponsor").addClass("play");
 	window.navigator.vibrate([30,10,20,10,10]);
 }, 3400);
 
 setTimeout(function () {
+	bamSound();
 	$("#intro .speaker").addClass("play");
 	window.navigator.vibrate([30,10,20,10,10]);
 }, 4000);
 
 setTimeout(function () {
+	bamSound();
 	$("#intro .updates").addClass("play");
 	window.navigator.vibrate([30,10,20,10,10]);
 }, 4600);
 
 setTimeout(function () {
+	bamSound();
+	bamSound2();
 	$("#intro .input").addClass("play");
 	window.navigator.vibrate([30,10,20,10,10]);
 }, 5200);
 
 setTimeout(function () {
+	bamSound();
 	window.navigator.vibrate([30,10,20,10,10]);
 }, 5800);
