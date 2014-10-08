67cb0b2c8d91888d6f55fe439fdb3a9ad1de4538
Thomas Gorissen <mail@serrynaimo.com>
2014-10-08 13:46:39 +0800

    Adding fb pixel

--- index.php
+++ index.php
@@ -414,5 +414,21 @@
 	  ga('send', 'pageview');
 
 	</script>
+<script type="text/javascript">
+(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+var fbds = document.createElement('script');
+fbds.async = true;
+fbds.src = '//connect.facebook.net/en_US/fbds.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(fbds, s);
+_fbq.loaded = true;
+}
+})();
+window._fbq = window._fbq || [];
+$("a[href='https://eventnook.com/event/register/22634']").click(function(){  window._fbq.push(['track', '6021415110904', {'value':'1','currency':'USD'}]); })
+
+</script>
 </body>
 </html>
